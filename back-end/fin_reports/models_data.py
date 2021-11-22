23eb6e4715d1d535bb4a60914d6010c1e62362fe
Yujihyeee <rladnddl@naver.com>
2021-11-22 19:29:53 +0900

    19

--- back-end/fin_reports/models_data.py
+++ back-end/fin_reports/models_data.py
@@ -2,6 +2,8 @@ import os
 import django
 import csv
 import sys
+
+import pandas as pd
 from icecream import ic
 from common.models import ValueObject, Printer, Reader
 # system setup
@@ -15,20 +17,26 @@ class DbUploader:
         reader = Reader()
         self.printer = Printer()
         vo.context = 'fin_reports/data/'
-        vo.fname='2020_PL.csv'
+        vo.fname='2020_PL_2.csv'
         self.csvfile = reader.new_file(vo)
 
     def insert_data(self):
         self.insert_fin_report()
 
     def insert_fin_report(self):
+        # # price['당기'] = price['당기'].fillna(0)
+        # # print(price.isnull().sum())
+        # df = pd.read_csv(self.csvfile, encoding='UTF-8', thousands=',')
+        # #  df['crime'] = df['살인 발생'] + df['강도 발생'] + df['강간 발생'] + df['절도 발생'] + df['폭력 발생']
+        # #  df['arrest'] = df.groupby(df['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거'])
+        # df.to_csv(self.csvfile + '2020_PL_2.csv')
         with open(self.csvfile, newline='', encoding='utf8') as f:
             data_reader = csv.DictReader(f)
-
             for row in data_reader:
-                if not FinReports.objects.filter(year=row['결산기준일']).exists():
-                    fin_reports = FinReports.objects.create(year=row['결산기준일'],
+                if not FinReports.objects.filter(category=row['항목명']).exists():
+                    fin_reports = FinReports.objects.create(year=2020,
                                                             category=row['항목명'],
                                                             price=row['당기'])
                     print(f'1 >>>> {fin_reports}')
+        #             row['결산기준일']
         print('USER DATA UPLOADED SUCCESSFULY!')
--- back-end/fin_reports/urls.py
+++ back-end/fin_reports/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
 
 from fin_reports import views
 
-urlpatterns = [
-    url(r'upload', views.upload)
-]
+urlpatterns = {
+    url(r'upload', views.upload),
+
+}
