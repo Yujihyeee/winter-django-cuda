4cdf41b46f7331ed7081886845a3561cc1212487
Yujihyeee <rladnddl@naver.com>
2021-12-06 23:38:21 +0900

    23

--- back-end/user/models.py
+++ back-end/user/models.py
@@ -3,13 +3,11 @@ from image.models import Category, Image
 
 
 class Person(models.Model):
-
     # use_in_migrations = True
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     age = models.IntegerField()
     gender = models.BooleanField()
     mbti = models.TextField()
-
 
     class Meta:
         db_table = "person"
@@ -19,7 +17,6 @@ class Person(models.Model):
 
 
 class User(models.Model):
-
     # use_in_migrations = True
     username = models.CharField(max_length=10)
     password = models.CharField(max_length=20)
