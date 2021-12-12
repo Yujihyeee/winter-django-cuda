cfaa01e3b9288ac61dbed0812c3ebaa8136259ad
Yujihyeee <rladnddl@naver.com>
2021-12-12 23:49:07 +0900

    23

--- back-end/fin_reports/models_process.py
+++ back-end/fin_reports/models_process.py
@@ -1,8 +1,14 @@
+from common.models import ValueObject, Reader, Printer
 
 
 class Processing:
     def __init__(self):
-        pass
+        vo = ValueObject()
+        reader = Reader()
+        self.printer = Printer()
+        vo.context = 'reservation/data/'
+        vo.fname = 'price.csv'
+        self.csvfile = reader.new_file(vo)
 
     def process(self):
         pass
